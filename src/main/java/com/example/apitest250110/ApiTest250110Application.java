5809c7625bbc8c6f384177f51d60b9b7b733f683
SeungHo <gemdoq@gmail.com>
2025-01-13 14:48:18 +0900

    Initial commit

---
+++ src/main/java/com/example/apitest250110/ApiTest250110Application.java
@@ -0,0 +1,13 @@
+package com.example.apitest250110;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTest250110Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTest250110Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/apitest250110/ApiTest250110ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apitest250110;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTest250110ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
